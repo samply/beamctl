14a4f84a9dfbd1b511b67bc9ca90728f014ca294
janskiba <jan.skiba@dkfz-heidelberg.de>
2023-07-20 07:59:05 +0000

    Return contol flow back to main from handler

--- src/proxy_health.rs
+++ src/proxy_health.rs
@@ -1,15 +1,17 @@
-use std::{time::SystemTime, process::exit};
+use std::time::SystemTime;
 
-use anyhow::{Result, Context};
+use anyhow::{Result, Context, anyhow};
 use reqwest::{Client, StatusCode, Url};
 use serde::{Serialize, Deserialize};
+
+use crate::icinga::IcingaCode;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct ProxyStatus {
     last_active: SystemTime
 }
 
-pub async fn query_proxy_health(proxy_name: &str, api_key: &str, broker_url: &Url) -> Result<()> {
+pub async fn query_proxy_health(proxy_name: &str, api_key: &str, broker_url: &Url) -> Result<IcingaCode> {
     let client = Client::new();
     let mut url = broker_url.clone();
     url.set_path(&format!("v1/health/proxies/{proxy_name}"));
@@ -18,11 +20,10 @@ pub async fn query_proxy_health(proxy_name: &str, api_key: &str, broker_url: &Ur
     match res.status() {
         StatusCode::NOT_FOUND => {
             println!("Proxy {proxy_name} never reported to the broker!");
-            exit(2);
+            Ok(IcingaCode::Critical)
         },
         StatusCode::UNAUTHORIZED => {
-            println!("Invalid monitoring apikey!");
-            exit(13);
+            Err(anyhow!("Invalid monitoring apikey!"))
         },
         StatusCode::SERVICE_UNAVAILABLE => {
             let status: ProxyStatus = res.json().await.unwrap();
@@ -30,15 +31,14 @@ pub async fn query_proxy_health(proxy_name: &str, api_key: &str, broker_url: &Ur
             let minutes = last_report_dur.as_secs() / 60;
             let seconds = last_report_dur.as_secs() % 60;
             println!("Beam.Proxy unavailable: last report was {minutes}m and {seconds}s ago!");
-            exit(2);
+            Ok(IcingaCode::Critical)
         }
         StatusCode::OK => {
             println!("Beam.Proxy is connected to the Broker.");
-            exit(0);
+            Ok(IcingaCode::Ok)
         },
         unexpectd => {
-            println!("Got unexpected statuscode {unexpectd} from broker");
-            exit(13);
+            Err(anyhow!("Got unexpected statuscode {unexpectd} from broker"))
         }
     }
 }
